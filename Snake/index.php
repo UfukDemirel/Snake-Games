e6fe224541ede4807c864f4975ebce040ee5fb78
UfukDemirel <85198386+UfukDemirel@users.noreply.github.com>
2022-07-07 12:09:59 +0300

    Snake Games

---
+++ Snake/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container noselect">
+    <div class="wrapper">
+        <button id="replay">
+            <i class="fas fa-play"></i>
+            RESTART
+        </button>
+        <div id="canvas">
+
+        </div>
+        <div id="ui">
+            <h2>SCORE
+            </h2>
+            <span id="score">00</span>
+        </div>
+    </div>
+    <div id="author">
+        <h1>SNAKE</h1> <span>by Fariat</span>
+    </div>
+</div>
+
+</body>
+<script src="script.js"></script>
+</html>
